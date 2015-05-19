c2629a4f70a4ccc34b633a16c586b83ad2e2499f
tom <tommaso@pavese.me>
2015-05-19 12:48:58 +0100

    added minitest gem

--- test/prime_numbers_test.rb
+++ test/prime_numbers_test.rb
@@ -1,6 +1,6 @@
 require 'minitest_helper'
 
-class TestPrimeNumbers < Minitest::Test
+class PrimeNumbersTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::PrimeNumbers::VERSION
   end
