8b0a2b5c14fc53abd0390f40a0b18018bf2ca2a2
tom <tommaso@pavese.me>
2015-05-20 02:07:26 +0100

    added custom algorithm to calculate prime numbers

--- lib/prime_numbers/number_generator.rb
+++ lib/prime_numbers/number_generator.rb
@@ -3,9 +3,33 @@ require 'prime'
 module PrimeNumbers
   module NumberGenerator
     class << self
+
       def get(count)
-        Prime.first count
+        if count > 1
+          generate(count)
+        elsif count == 1
+          [2]
+        else
+          []
+        end
       end
+
+
+      private
+
+
+      def generate(count)
+        primes = [2, 3]
+        last = 3
+
+        until primes.length == count
+          last += 2
+          tests = 2..Math.sqrt(last).ceil
+          primes << last unless tests.any? { |test| last % test == 0 }
+        end
+        primes
+      end
+
     end
   end
 end
