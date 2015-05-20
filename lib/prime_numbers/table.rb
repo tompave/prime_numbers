b3ff7f1154aeafb42ba69f9911c8322cc7113cf5
tom <tommaso@pavese.me>
2015-05-20 03:34:32 +0100

    simplified the Table interface

--- lib/prime_numbers/table.rb
+++ lib/prime_numbers/table.rb
@@ -3,22 +3,16 @@ module PrimeNumbers
     attr_reader :primes, :size, :columns
 
     def initialize(array)
-      @primes = array
-      @size = primes.length
-      populate!
-    end
-
-
-    def [](index)
-      columns[index]
+      @primes  = array
+      @size    = primes.length
+      @columns = build_columns
     end
 
 
     private
 
-
-    def populate!
-      @columns = primes.map do |x|
+    def build_columns
+      primes.map do |x|
         products = primes.map do |y|
           Product.new(x, y)
         end
--- test/table_test.rb
+++ test/table_test.rb
@@ -26,16 +26,4 @@ class TableTest < Minitest::Test
     assert_equal @list.length, @table.columns[0].products.size
   end
 
-
-  def test_array_access
-    assert_instance_of PrimeNumbers::Column, @table[0]
-  end
-
-
-  def test_deep_array_access
-    assert_instance_of PrimeNumbers::Product, @table[0][0]
-
-    assert_equal 4, @table[0][0].value
-    assert_equal 10, @table[2][0].value
-  end
 end
